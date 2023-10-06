4d21ff3f4ad471304b61ba91569d595949ffa408
Essel Luna <essel.luna1@trans-cosmos.co.jp>
2023-10-06 13:49:15 +0800

    update html css and js file

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// const shareButton = document.querySelector('icon');
+// const shareBox = document.querySelector('shareList');
+
+// shareButton.addEventListener('click', () => {
+//   shareBox.classList.toggle('hidden');
+// })
+
+var shareButton = document.querySelector('.shareIcon')
+var share = document.querySelector('.share');
+var shareLinks = document.querySelector('.shareList');
+
+shareButton.addEventListener('click', function () {
+    shareLinks.parentElement.classList.toggle('active');
+
+    if (shareButton.classList.contains("active")) {
+    shareButton.classList.replace("active", "inactive");
+    } else {
+    shareButton.classList.add("active");
+    }
+})
